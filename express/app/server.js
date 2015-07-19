7315686fd634d5ba1e74b15081d9ae95189d35de
hirekris <khebert@KristoffersMBP3.hsd1.wa.comcast.net>
2015-07-18 18:25:43 -0700

    adding express jasmine

--- express/app/server.js
+++ express/app/server.js
@@ -2,10 +2,22 @@ var express = require('express');
 var app = express();
 var request = require('request');
 
+function isAdmin(req, res, next) {
+    console.log(req);
+    if (req.user.isAdmin === 'undefined') {
+    };
+    next();
+}
+
 app.get('/', function getHome(req, res){
-    res.send('success');
+    res.status(200).send('success');
 });
 
-console.log('server starting on port 3000');
-app.listen(3000)
+app.get('/admin', function getAdmin(req, res){
+    res.status(403).send('forbidden');
+
+});
+
+console.log('server starting on port 3002');
+app.listen(3002)
 
--- express/spec/serverSpec.js
+++ express/spec/serverSpec.js
@@ -1,35 +1,25 @@
 'use strict';
 
-function isDefined() {
-    it('Expect app to be defined', function appIsdefinedfunction() {
-        expect(app).toBeDefined();
-    });
-}
+var request = require('request');
 
-function getTotal() {
-    it('Expect app total method to add inputs', function addsTwoInputsTogether() {
-        expect(app.getTotal(1,2)).toBe(3);
-    });
-}
-
-function homepageReturns200(done) {
-    it('get homepage and expect 200', function homePageReturns200(){
-        request('http://localhost:3000/', function testResponse(err, response, body){
-            expect(response.statusCode)toBe(200);
+function getHome() {
+    it('get homepage and expect 200', function homePageReturns200(done){
+        request.get('http://localhost:3002/', function testResponse(err, response, body){
+            expect(response.statusCode).toBe(200);
             done();
 
         });
     })
 }
 
-function protectedPageReturns403(done) {
-    it('get homepage and expect 200', function homePageReturns200(){
-        request('http://localhost:3000/', function testResponse(err, response, body){
-            expect(response.statusCode)toBe(200);
+function getAdmin() {
+    it('get admin and expect 403', function getAdminReturns403(done){
+        request.get('http://localhost:3002/admin', function testResponse(err, response, body){
+            expect(response.statusCode).toBe(403);
             done();
         });
     })
 }
 
-describe("app is defined", isDefined);
-describe("app adds total", getTotal);
+describe("Test home page", getHome);
+describe("Test admin urls", getAdmin);
